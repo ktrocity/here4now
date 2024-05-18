1ccd8d8a704ff4cbab6cb49a484519823ffd352d
Keli Rice <82687494+ktrocity@users.noreply.github.com>
2024-05-17 21:18:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# here4now
+ learnREACT
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Note from './Note';
+
+function App() {
+    return <div>
+        <Note></Note>
+        <Note></Note>
+    </div>
+}
+
+export default App;
